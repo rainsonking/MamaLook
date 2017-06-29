b6499ff90281141731f64fd3279dcbd7a7211e3a
Administrator <383574622@qq.com>
2017-06-29 22:07:20 +0800

    循环消息完成

--- app/src/main/java/kehuhuaPage/fragments/HomePageFragment.java
+++ app/src/main/java/kehuhuaPage/fragments/HomePageFragment.java
@@ -1,5 +1,6 @@
 package kehuhuaPage.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import kehuhuaPage.widgts.CnToolbar;
+import kehuhuaPage.widgts.ScrollText;
 import razerdp.friendcircle.R;
 
 
@@ -31,8 +33,12 @@ public class HomePageFragment extends Fragment implements View.OnClickListener {
         CnToolbar cnToolbar = (CnToolbar) view.findViewById(R.id.stu_toolbar);
         cnToolbar.setTitle("主页");
         initLocalBanner(view);
+        initScrollText(view);
         return view;
     }
+
+
+
     @Override
     public void onClick(View v) {
 
@@ -60,7 +66,42 @@ public class HomePageFragment extends Fragment implements View.OnClickListener {
         });
     }
 
+   private ScrollText mTextview;
+    private void initScrollText(View view) {
+        mTextview = (ScrollText) view.findViewById(R.id.text_scroll_home_page);
+
+        ArrayList<String> mTips= new ArrayList<>();
+        mTips.add("恭喜大王喜当爹");
+        mTips.add("6666666666666666666666666666666666666666666666");
+        mTips.add("soga");
+        mTextview.setTextList(mTips);//加入显示内容,集合类型
+        mTextview.setText(26, 5, Color.RED);//设置属性,具体跟踪源码
+        mTextview.setTextStillTime(3000);//设置停留时长间隔
+        mTextview.setAnimTime(300);//设置进入和退出的时间间隔
+        //对单条文字的点击监听
+        mTextview.setOnItemClickListener(new ScrollText.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                toast("第" + (position+1) +"被点中了");
+            }
+        });
+    }
+
+
     private void toast(String str) {
         Toast.makeText(getActivity(), str, Toast.LENGTH_SHORT).show();
+    }
+
+    //开始滚动
+    @Override
+    public void onResume() {
+        super.onResume();
+        mTextview.startAutoScroll();
+    }
+    //停止滚动
+    @Override
+    public void onPause() {
+        super.onPause();
+        mTextview.stopAutoScroll();
     }
 }
